d050d04f54ad97e8a79219572d60fb8f63a16d7c
dangHT <dang@marble-d.co.jp>
2021-12-13 21:35:23 +0700

    v1

--- src/store/modules/account.js
+++ src/store/modules/account.js
@@ -5,181 +5,151 @@ const state = () => ({
     {
         'email': 'taliyahholland4620@egg-mail.co',
         'pass': 'egg-mail.co@!012!@',
-        'ingameName': 'taliyahholland46',
-        'private_key': 'rrbvQtFmW0Yp70QZpkFtu36cQtvypXUS'
+        'ingameName': 'taliyahholland46'
     },
     {
         'email': 'rowangilmore9452@egg-mail.co',
         'pass': 'egg-mail.co@!012!@',
-        'ingameName': 'rowangilmore',
-        'private_key': 'RvXRYfG3PBIgh9dbcGdGPtwpoDGQ0AlN'
+        'ingameName': 'rowangilmore'
     },
     {
         'email': 'victorklein4788@egg-mail.co',
         'pass': 'egg-mail.co@!012!@',
-        'ingameName': 'victorklein4788',
-        'private_key': 'S3s4TKX6THvVKTxS7csFck5ynQm6qgf0'
+        'ingameName': 'victorklein4788'
     },
     {
         'email': 'lucaclairexhiw@gmail.com',
-        'pass': 'danghuynh',
-        'private_key': '5KP8wqWRy6NyR1Djuz3LUf9hYgNCurFjxE6Rv2iGWdR98XsgkRd',
+        'pass': '80@Minhkhai',
         'ingameName': 'lucaclairex'
     },
     {
         'email': 'lelandnorton6075@egg-mail.co',
         'pass': 'egg-mail.co@!012!@',
-        'ingameName': 'lelandnorton6075',
-        'private_key': 'Sf2gqE4Bho6rFRSpbDfPjjhYvlIj39os'
+        'ingameName': 'lelandnorton6075'
     },
     {
         'email': 'jamisongould4497@z-mail.vip',
         'pass': 'z-mail.vip@!012!@',
-        'ingameName': 'jamisongould4497',
-        'private_key': 'SI876aCn9JjSP9XRWn7DYPBeOAR1Nz45'
+        'ingameName': 'jamisongould4497'
     },
     {
         'email': 'suttonliamziga@gmail.com',
-        'pass': 'danghuynh',
-        'private_key': '5KUaD6myJNfZmYuGV6QGR1y6oWvEkTAJEiYnjJ6aWTavPBHiigX',
+        'pass': '80@Minhkhai',
         'ingameName': 'byneg'
     },
     {
         'email': 'zaneclayton8992@gmail.com',
-        'pass': 'danghuynh',
-        'private_key': '5JYq1JBhx3PaxtvHRMCS2bTTrGVaHSZYCL81rD6LTPGUJM89BXe',
+        'pass': '80@Minhkhai',
         'ingameName': 'zaneclayton8'
     },
     {
         'email': 'zaireabdullah265@gmail.com',
-        'pass': 'danghuynh',
-        'private_key': '5KBg5jGSuiwZMoYDuSJRtKZsxX5zdqMTmUqtfEA8t91xzQUUQLW',
+        'pass': '80@Minhkhai',
         'ingameName': 'zaireabdulla'
     },
     {
         'email': 'jakehebert6930@z-mail.vip',
         'pass': 'z-mail.vip@!012!@',
-        'ingameName': 'jakehebert6930',
-        'private_key': 'QglVZueHBQtTqPAtAN0xJwvQ7LEui6SQ'
+        'ingameName': 'jakehebert6930'
     },
     {
         'email': 'alenaashley7702@egg-mail.co',
         'pass': 'egg-mail.co@!012!@',
-        'ingameName': 'alenaashley7702',
-        'private_key': 'qkY4t71JrfuJqTWWwft9ixdFzU0ZQuuI'
+        'ingameName': 'alenaashley7702'
     },
     {
         'email': 'darrylkeith6265@egg-mail.co',
         'pass': 'egg-mail.co@!012!@',
-        'ingameName': 'darrylkeith6265',
-        'private_key': 'qprnKlNqG1PizEyfXEIXr5PqEPEboYon'
+        'ingameName': 'darrylkeith6265'
     },
     {
         'email': 'axelrice2220@z-mail.vip',
         'pass': 'z-mail.vip@!012!@',
-        'ingameName': 'axelrice2220',
-        'private_key': 'qptJnSfZ2DNcDeFMICIB9XCV8urpTkLz'
+        'ingameName': 'axelrice2220'
     },
     {
         'email': 'ericwilliamson9282@z-mail.vip',
         'pass': 'z-mail.vip@!012!@',
-        'ingameName': 'ericwilmson9282',
-        'private_key': 'QronD38nkR341rYXl9LnaYJ8GAt1GoCb'
+        'ingameName': 'ericwilmson9282'
     },
     {
         'email': 'genevievenelson7389@z-mail.vip',
         'pass': 'z-mail.vip@!012!@',
-        'ingameName': 'genevievenel7389',
-        'private_key': 'Qv2W4SCDr1UKCbg6YQsZeAmrAGmZGatX'
+        'ingameName': 'genevievenel7389'
     },
     {
         'email': 'marelifarrell8962@egg-mail.co',
         'pass': 'egg-mail.co@!012!@',
-        'ingameName': 'marelifarrell8',
-        'private_key': 'rfOBTstH7MxCA3HHK8hh7hXWes4qKgft'
+        'ingameName': 'marelifarrell8'
     },
     {
         'email': 'johnathonharrington6308@egg-mail.co',
         'pass': 'egg-mail.co@!012!@',
-        'ingameName': 'johnathonharr',
-        'private_key': 'Rg2pm7DSUSs98DOMngV8JlvYo5bqVJe3'
+        'ingameName': 'johnathonharr'
     },
     {
         'email': 'katieesparza3555@z-mail.vip',
         'pass': 'z-mail.vip@!012!@',
-        'ingameName': 'katieesparza3555',
-        'private_key': 'RGPmwBKhhaoxYqkN61Kfsjxnwzs2jWYh'
+        'ingameName': 'katieesparza3555'
     },
     {
         'email': 'giovannisimon8839@z-mail.vip',
         'pass': 'z-mail.vip@!012!@',
-        'ingameName': 'giovanon8839',
-        'private_key': 'rHvkreBo3091TRLvvnpj0yiQirQuJyV2'
+        'ingameName': 'giovanon8839'
     },
     {
         'email': 'adalynnhester4654@z-mail.vip',
         'pass': 'z-mail.vip@!012!@',
-        'ingameName': 'adalynter4654',
-        'private_key': 'ridSnUuwhD7tZPKXUfInDPHM4s6c4Np2'
+        'ingameName': 'adalynter4654'
     },
     {
         'email': 'kadehayes4297@egg-mail.co',
         'pass': 'egg-mail.co@!012!@',
-        'ingameName': 'kadehayes4297',
-        'private_key': 'RMd9aRB7O8Gri7iL1zhOpLZw9DiHgRdd'
+        'ingameName': 'kadehayes4297'
     },
     {
         'email': 'simeoncross9322@z-mail.vip',
         'pass': 'z-mail.vip@!012!@',
-        'ingameName': 'simeoncross9322',
-        'private_key': 'RMk3q0KLYF4gejTgXhyWQUhXOCcmslGg'
+        'ingameName': 'simeoncross9322'
     },
     {
         'email': 'lilyanadawson5947@egg-mail.co',
         'pass': 'egg-mail.co@!012!@',
-        'ingameName': 'lilyana947',
-        'private_key': 'rNmX9z8Vdag89oGHOgKqyrzzEJZoJ6PX'
+        'ingameName': 'lilyana947'
     },
     {
         'email': 'lindacisneros6925@egg-mail.co',
         'pass': 'egg-mail.co@!012!@',
-        'ingameName': 'lindacisneros6',
-        'private_key': 'roQXpB1n5h5nZhRECJImyRGdj6N6rxSH'
+        'ingameName': 'lindacisneros6'
     },
     {
         'email': 'madalynnmartin8535@egg-mail.co',
         'pass': 'egg-mail.co@!012!@',
-        'ingameName': 'madalynnma',
-        'private_key': 'rP0DhXR4DPRitJEbFrWBCN1X3wNrhDLS'
+        'ingameName': 'madalynnma'
     },
     {
         'email': 'gunnerdavidson8368@z-mail.vip',
         'pass': 'z-mail.vip@!012!@',
-        'ingameName': 'gunnerdaon8368',
-        'private_key': 'rpfo9QBaLzl6EltoLela6w9DZgiys8Oj'
+        'ingameName': 'gunnerdaon8368'
     },
     {
         'email': 'caraspears6851@z-mail.vip',
         'pass': 'z-mail.vip@!012!@',
-        'ingameName': 'caraspears6851',
-        'private_key': 'RPPbEvNYmLfQZgKKtEB8cTwLSl3qhcYR'
+        'ingameName': 'caraspears6851'
     },
     {
         'email': 'kenyonbush6145@egg-mail.co',
         'pass': 'egg-mail.co@!012!@',
-        'ingameName': 'kenyonbush6145',
-        'private_key': 'rPpT2lVT08898iTxMQpsTxIppBTCguYJ'
+        'ingameName': 'kenyonbush6145'
     },
     {
         'email': 'jaylandavis7804@z-mail.vip',
         'pass': 'z-mail.vip@!012!@',
-        'ingameName': 'jaylandavis7804',
-        'private_key': 'RPwgELyEQ2QUeWUR7JzxuTTgHrWEIeEK'
+        'ingameName': 'jaylandavis7804'
     },
     {
         'email': 'thaliahadleydk12@gmail.com',
-        'pass': 'danghuynh',
-        'private_key': '5K812TgWsnECyVzekpcRgMQayQRMrhKqLgcRG6VWbeSMxG45FWU',
+        'pass': '80@Minhkhai',
         'ingameName': 'ncovt4'
     }
     ],
@@ -187,151 +157,126 @@ const state = () => ({
         {
             'email': 'SONVNN1@HASPLINTER.XYZ',
             'pass': '80@Minhkhai',
-            'private_key': 'Wdux3p3cmmob8N1Auhm1MFELzexyjxz8',
             'ingameName': 'sonvnn1'
         },
         {
             'email': 'SONVNN2@HASPLINTER.XYZ',
             'pass': '80@Minhkhai',
-            'private_key': 'zXqwt4LhHDhaSpNkNFypKWL58TW3PpmD',
             'ingameName': 'sonvnn2'
         },
         {
             'email': 'SONVNN3@HASPLINTER.XYZ',
             'pass': '80@Minhkhai',
-            'private_key': '2qARSj9BbpzR2ZfHNnPSZvsBgon21UCf',
             'ingameName': 'sonvnn3'
         },
         {
             'email': 'SONVNN4@HASPLINTER.XYZ',
             'pass': '80@Minhkhai',
-            'private_key': 'jiysgt0jwa7nEz5GVN2DnB1ODQP5K8q3',
             'ingameName': 'sonvnn4'
         },
         {
             'email': 'SONVNN5@HASPLINTER.XYZ',
             'pass': '80@Minhkhai',
-            'private_key': 'wxoMIYAHg5wIsEkxkhpDN80jKbc5cit2',
             'ingameName': 'sonvnn5'
         },
         {
             'email': 'SONVNN6@HASPLINTER.XYZ',
             'pass': '80@Minhkhai',
-            'private_key': 'aCfj0wUMfXbyXXJfTVEhPjU6V4R4Rjok',
             'ingameName': 'sonvnn6'
         },
         {
             'email': 'SONVNN7@HASPLINTER.XYZ',
             'pass': '80@Minhkhai',
-            'private_key': 'ovob62hvZKzLiW2CA3wE7ChxqTXVi7FS',
             'ingameName': 'sonvnn7'
         },
         {
             'email': 'SONVNN8@HASPLINTER.XYZ',
             'pass': '80@Minhkhai',
-            'private_key': 'CaLY4DpS7kwLPBqXngzavHspYkTD0rQm',
             'ingameName': 'sonvnn8'
         },
         {
             'email': 'SONVNN9@HASPLINTER.XYZ',
             'pass': '80@Minhkhai',
-            'private_key': 'qkrW59LszXrujxP6tbPBfKcvOCXD5T0a',
             'ingameName': 'sonvnn9'
         },
         {
             'email': 'SONVNN10@HASPLINTER.XYZ',
             'pass': '80@Minhkhai',
-            'private_key': 'ITVBM3aWE9fJsHtm7mqD1x4FwkqVPIo1',
             'ingameName': 'sonvnn10'
         },
         {
             'email': 'SONVNN11@HASPLINTER.XYZ',
             'pass': '80@Minhkhai',
-            'private_key': 'HAvdqFbR8neDXnpSQIfhvHs9rYp1nTd7',
             'ingameName': 'sonvnn11'
         },
         {
             'email': 'SONVNN12@HASPLINTER.XYZ',
             'pass': '80@Minhkhai',
-            'private_key': '8oETdii5SFeaqhpt1LO5TVlZFLHHX7tI',
             'ingameName': 'sonvnn12'
         },
         {
             'email': 'SONVNN13@HASPLINTER.XYZ',
             'pass': '80@Minhkhai',
-            'private_key': 'Gzro6be1Tfnq7OLtmylZYIVQeV7iOgJQ',
             'ingameName': 'sonvnn13'
         },
         {
             'email': 'SONVNN14@HASPLINTER.XYZ',
             'pass': '80@Minhkhai',
-            'private_key': 'FoLb5dYNBRB8MycbaRB9MYV9QFbRpPmh',
             'ingameName': 'sonvnn14'
         },
         {
             'email': 'SONVNN15@HASPLINTER.XYZ',
             'pass': '80@Minhkhai',
-            'private_key': 'BwPd4BxNUmaXvX7GJJP44cMgxC8Vf0qv',
             'ingameName': 'sonvnn15'
         },
         {
             'email': 'SONVNN16@HASPLINTER.XYZ',
             'pass': '80@Minhkhai',
-            'private_key': 'pmZsl5t7GXNjc7zPTHw042EMwZujK2TB',
             'ingameName': 'sonvnn16'
         },
         {
             'email': 'SONVNN17@HASPLINTER.XYZ',
             'pass': '80@Minhkhai',
-            'private_key': 'bv7TdkOTrDhnETmQcnHUYT97H0f37Bi2',
             'ingameName': 'sonvnn17'
         },
         {
             'email': 'SONVNN18@HASPLINTER.XYZ',
             'pass': '80@Minhkhai',
-            'private_key': 'zrqHUjfxEaN1LG1H1V5yOf2B4yWKRHhu',
             'ingameName': 'sonvnn18'
         },
         {
             'email': 'SONVNN19@HASPLINTER.XYZ',
             'pass': '80@Minhkhai',
-            'private_key': 'ARQsHR1IUJQ2ps7qJFDXqcvAdEPxcAJG',
             'ingameName': 'sonvnn19'
         },
         {
             'email': 'SONVNN20@HASPLINTER.XYZ',
             'pass': '80@Minhkhai',
-            'private_key': 'ywhuHH53tvpnpkPrRchSUuMrmDJP9r6k',
             'ingameName': 'sonvnn20'
         },
         {
             'email': 'SONVNN21@HASPLINTER.XYZ',
             'pass': '80@Minhkhai',
-            'private_key': 'EqBWXJtI0YQTbNrbSeyzUMzemgV7uY4n',
             'ingameName': 'sonvnn21'
         },
         {
             'email': 'SONVNN22@HASPLINTER.XYZ',
             'pass': '80@Minhkhai',
-            'private_key': 'CRi8ma9OAlYWrOEM2syEwufq7d6sNGm1',
             'ingameName': 'sonvnn22'
         },
         {
             'email': 'SONVNN23@HASPLINTER.XYZ',
             'pass': '80@Minhkhai',
-            'private_key': 'JhueGmwPIN22kNxafoviRfNPL2DAs6Wc',
             'ingameName': 'sonvnn23'
         },
         {
             'email': 'SONVNN24@HASPLINTER.XYZ',
             'pass': '80@Minhkhai',
-            'private_key': '9feJl0TuHioDaSWPzOU9c5v1h7ZSQjdA',
             'ingameName': 'sonvnn24'
         },
         {
             'email': 'SONVNN25@HASPLINTER.XYZ',
             'pass': '80@Minhkhai',
-            'private_key': 'EjfNxenHnsf1XNzmwB21Tjew4YPRbH1Z',
             'ingameName': 'sonvnn25'
         }
     ]
